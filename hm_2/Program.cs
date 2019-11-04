03037496a7473ad58f9e1684b26c2c74ca13db42
kmac3063 <lantcov.iiu@students.dvfu.ru>
2019-11-04 13:49:06 +1000

    Refactor

--- hm_2/Program.cs
+++ hm_2/Program.cs
@@ -4,72 +4,148 @@ namespace hm_2
 {
     public class Program
     {
-        const int REQ_ADMIN = 1;
-        const int REQ_MODERATOR = 2;
-        const int REQ_CLIENT = 3;
-        const int REQ_WORKER = 4;
+        const int REQ_Admin = 1;
+        const int REQ_Moderator = 2;
+        const int REQ_Client = 3;
+        const int REQ_Worker = 4;
         const int REQ_LOOKER = 5;
         const int REQ_EXIT = 6;
-        const int REQ_MANAGE_MODERATOR = 7;
+
+        const int REQ_MANAGE_Moderator = 7;
         const int REQ_MANAGE_REQ_AND_OF = 8;
         const int REQ_GO_BACK = 9;
         //const int REQ_EXIT = 6;
         //const int REQ_EXIT = 6;
 
-        static void read_request(ref int request)
+        static void read_request(ref int request, string status)
         {
-            Console.WriteLine("\n\nОт чьего лица действовать: ");
-            Console.WriteLine("1. Глав.админ");
-            Console.WriteLine("2. Модератор");
-            Console.WriteLine("3. Заказчик");
-            Console.WriteLine("4. Исполнитель");
-            Console.WriteLine("5. Просто смотрю");
-            Console.WriteLine("6. Выйти");
-            Console.Write("\nВведите номер: ");
+            switch (status)
+            {
+                case ("base"):
+                    Console.WriteLine("\n\nОт чьего лица действовать: ");
+                    Console.WriteLine("1. Глав.админ");
+                    Console.WriteLine("2. Модератор");
+                    Console.WriteLine("3. Заказчик");
+                    Console.WriteLine("4. Исполнитель");
+                    Console.WriteLine("5. Просто смотрю");
+                    Console.WriteLine("6. Выйти");
+                    Console.Write("\nВведите номер: ");
+                    break;
+                case ("admin"):
+                    Console.WriteLine("\n\nЧто вы хотите сделать?: ");
+                    Console.WriteLine("1. Посмотреть список модераторов");
+                    Console.WriteLine("2. Добавить модератора");
+                    Console.WriteLine("3. Удалить модератора");
+                    Console.WriteLine("4. Посмотреть список фрилансеров");
+                    Console.WriteLine("5. Посмотреть список заказчиков");
+                    Console.WriteLine("6. Посмотреть список заявлений(поиск клиента)");
+                    Console.WriteLine("7. Одобрить заявление(поиск клиента)");
+                    Console.WriteLine("8. Посмотреть список заявлений(поиск исполнителя)");
+                    Console.WriteLine("9. Одобрить заявление(поиск клиента)");
+                    Console.WriteLine("10. Посмотреть имя админа");
+                    Console.WriteLine("11. Выйти");
 
+                    Console.Write("\nВведите номер: ");
+                    break;
+                case ("moderator"):
+                    Console.WriteLine("\n\nЧто вы хотите сделать?: ");
+                    Console.WriteLine("1. Посмотреть список модераторов");
+                    Console.WriteLine("2. Посмотреть список фрилансеров");
+                    Console.WriteLine("3. Посмотреть список заказчиков");
+                    Console.WriteLine("4. Посмотреть список заявлений(поиск клиента)");
+                    Console.WriteLine("5. Одобрить заявление(поиск клиента)");
+                    Console.WriteLine("6. Посмотреть список заявлений(поиск исполнителя)");
+                    Console.WriteLine("7. Одобрить заявление(поиск клиента)");
+                    Console.WriteLine("8. Посмотреть имя админа");
+                    Console.WriteLine("9. Выйти");
+
+                    Console.Write("\nВведите номер: ");
+                    break;
+                case ("client"):
+                    Console.WriteLine("\n\nЧто вы хотите сделать?: ");
+                    Console.WriteLine("1. Добавить заявление(поиск исполнителя)");
+                    Console.WriteLine("2. Посмотреть список заявлений(поиск исполнителя)");
+                    Console.WriteLine("3. Удалить своё заявление(поиск исполнителя");
+                    Console.WriteLine("4. Посмотреть список фрилансеров");
+                    Console.WriteLine("5. Выбрать услугу фрилансера");
+                    Console.WriteLine("6. Посмотреть имя админа");
+                    Console.WriteLine("7. Выйти");
+
+                    Console.Write("\nВведите номер: ");
+                    break;
+                case ("worker"):
+                    Console.WriteLine("\n\nЧто вы хотите сделать?: ");
+                    Console.WriteLine("1. Добавить заявление(поиск заказчика)");
+                    Console.WriteLine("2. Посмотреть список заявлений(поиск заказчика)");
+                    Console.WriteLine("3. Удалить своё заявление(поиск заказчика");
+                    Console.WriteLine("4. Посмотреть список заказчиков");
+                    Console.WriteLine("5. Выполнить запрос заказчика");
+                    Console.WriteLine("6. Посмотреть имя админа");
+                    Console.WriteLine("7. Выйти");
+
+                    Console.Write("\nВведите номер: ");
+                    break;
+                case ("look"):
+                    Console.WriteLine("\n\nЧто вы хотите сделать?: ");
+                    Console.WriteLine("1. Посмотреть список фрилансеров");
+                    Console.WriteLine("2. Посмотреть список заказчиков");
+                    Console.WriteLine("3. Посмотреть имя админа");
+                    Console.WriteLine("4. Выйти");
+
+                    Console.Write("\nВведите номер: ");
+                    break;
+            }
+            
             request = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+        }
+
+        void read_Admin_request() //TO DO
+        {
+            
+
+            //request = Int32.Parse(Console.ReadLine());
             Console.WriteLine("\n");
         }
         static void Main(string[] args)
         {
             Console.Write("Введите имя админа: ");
-            var adm_name = Console.ReadLine();
+            Admin admin = new Admin(Console.ReadLine());
 
-            freelanceExchange freelanceExchange = new freelanceExchange(adm_name);
+            Moderator moderator = new Moderator("Миша");
+
+            People people = new People("Серёжа");
+            Client client = new Client("Снежана");
+            Worker worker = new Worker("Игорь");
+
+            FreelanceExchange FreelanceExchange = new FreelanceExchange(admin);
 
             var request = 0;
             while (request != REQ_EXIT)
             {
-                read_request(ref request);
+
+                read_request(ref request, "base");
                 switch (request)
                 {
-                    case REQ_ADMIN:
+                    case REQ_Admin:
                         {
                             Console.WriteLine("------------------------------");
                             Console.WriteLine("ВЫ АДМИН");
                             Console.WriteLine("------------------------------\n");
                             while (request != REQ_GO_BACK)
                             {
-                                switch (read_admin_request())
+                                read_request(ref request, "admin");
+                                switch (request)
                                 {
-                                    case REQ_LOOKER:
-                                        lookInfo(freelanceExchange, "ADMIN");
-                                        break;
-                                    case REQ_MANAGE_MODERATOR:
-                                        manage_moderator(freelanceExchange);
-                                        break;
-                                    case REQ_MANAGE_REQ_AND_OF:
-                                        manage_request_and_offers(freelanceExchange);
-                                        break;
-                                    default:
-                                        request = REQ_GO_BACK;
+                                    case (1):
+                                        //admin.
                                         break;
                                 }
                             }
                             
                             break;
                         }
-                    case REQ_MODERATOR:
+                    case REQ_Moderator:
                         {
                             Console.WriteLine("------------------------------");
                             Console.WriteLine("ВЫ МОДЕРАТОР");
@@ -81,7 +157,7 @@ namespace hm_2
                             //Удалить жанр
                             break;
                         }
-                    case REQ_CLIENT:
+                    case REQ_Client:
                         {
                             Console.WriteLine("------------------------------");
                             Console.WriteLine("ВЫ ЗАКАЗЧИК");
@@ -91,7 +167,7 @@ namespace hm_2
                             //Добавить заявку
                             break;
                         }
-                    case REQ_WORKER:
+                    case REQ_Worker:
                         {
                             Console.WriteLine("------------------------------");
                             Console.WriteLine("ВЫ ФРИЛАНСЕР");
--- hm_2/freelance.cs
+++ hm_2/freelance.cs
@@ -4,104 +4,186 @@ using System.Text;
 
 namespace hm_2
 {
-    public class people
+    public class People
     {
-        float balance;
-        public string name;
-    }
-    public class admin : moderator
-    {
-
-    }
-
-    public class moderator : people
-    {
-
-    }
-
-    public class client : people
-    {
-
-    }
-
-    public class worker : people
-    {
-
-    }
-    public class freelanceExchange
-    {
-        admin admin_;
-        moderator[] moderatorList_;
-        
-        worker[] ListOfWorkers_;
-        string[][] ListOfOffers_;
-
-        client[] ListOfClients_;
-        string[][] ListOfRequests_;
-
-        public freelanceExchange(string AdminName)
+        protected string name;
+        public People(string name_)
         {
-            admin_.name = AdminName;
+            name = name_;
+        }
+        
+
+        public string getName() { return name; }
+        
+        Admin getАdmin(FreelanceExchange FE) { return FE.admin; }
+        //public List<Worker> getListOfWorkers(FreelanceExchange FE) { return FE.listOfWorkers; }
+        //public List<Client> getListOfClient(FreelanceExchange FE) { return FE.listOfClients; }
+
+    }
+    public class Admin : Moderator
+    {
+        public Admin(string name_) : base(name_) { }
+
+        public void addModerator(ref FreelanceExchange FE, Moderator mod)
+        {
+            if (!FE.moderatorList.Contains(mod))
+                FE.moderatorList.Add(mod);
+        }
+
+        public void delModerator(ref FreelanceExchange FE, Moderator mod)
+        {
+            if (FE.moderatorList.Contains(mod))
+                FE.moderatorList.Remove(mod);
+        }
+    }
+
+    public class Moderator : People
+    {
+        
+        public Moderator(string name_) : base(name_) { }
+        List<Moderator> getModeratorList(FreelanceExchange FE) { return FE.moderatorList; }
+
+        public void addOffer(ref FreelanceExchange FE, int id)
+        {
+            FE.listOfOffers.Add(FE.waitOfferList[id]);
+            FE.waitOfferList.Remove(FE.waitOfferList[id]);
+        }
+
+        public void deleteOffer(ref FreelanceExchange FE, int id)
+        {
+            FE.listOfOffers.Remove(FE.listOfOffers[id]);
+        }
+
+        public void addRequest(ref FreelanceExchange FE, int id)
+        {
+            FE.listOfRequests.Add(FE.waitReqList[id]);
+            FE.waitReqList.Remove(FE.waitReqList[id]);
+        }
+
+        public void deleteRequest(ref FreelanceExchange FE, int id)
+        {
+            FE.listOfRequests.Remove(FE.listOfRequests[id]);
+        }
+    }
+
+    public class Client : People
+    {
+        public Client(string name_) : base(name_) { }
+
+        public void tryAddRequest(ref FreelanceExchange FE, List<string> req)
+        {
+            FE.waitReqList.Add(req);
+        }
+
+        public void delRequest(ref FreelanceExchange FE, List<string> req)
+        {
+            if (FE.waitReqList.Contains(req))
+                FE.waitReqList.Remove(req);
+            if (FE.listOfRequests.Contains(req))
+                FE.listOfRequests.Remove(req);
+        }
+        // req имя / что нужно / цена / сколько раз уже заказывал
+        // off имя / что могу / цена / стаж
+        public void buyOffer(ref FreelanceExchange FE, int id) {
+            FE.listOfOffers.Remove(FE.listOfOffers[id]);
+        }
+    }
+
+    public class Worker : People
+    {
+        public Worker(string name_) : base(name_) { }
+
+
+        public void tryAddOffer(ref FreelanceExchange FE, List<string> off)
+        {
+            FE.waitOfferList.Add(off);
+        }
+
+        public void delOffer(ref FreelanceExchange FE, List<string> off)
+        {
+            if (FE.waitOfferList.Contains(off))
+                FE.waitOfferList.Remove(off);
+            if (FE.listOfOffers.Contains(off))
+                FE.listOfOffers.Remove(off);
+        }
+        // req имя / что нужно / цена / сколько раз уже заказывал
+        // off имя / что могу / цена / стаж
+        public void doRequest(ref FreelanceExchange FE, int id)
+        {
+            FE.listOfRequests.Remove(FE.listOfRequests[id]);
+        }
+        //tryAddOffer
+        //pleaseDelOffer
+        //workReq
+    }
+    public class FreelanceExchange
+    {
+        public Admin admin;
+        public List<Moderator> moderatorList = new List<Moderator>();
+
+        //public List<Worker> listOfWorkers = new List<Worker>();
+        public List<List<string>> listOfOffers = new List<List<string>>();
+        public List<List<string>> listOfRequests = new List<List<string>>();
+
+        public List<List<string>> waitOfferList = new List<List<string>>();
+        public List<List<string>> waitReqList = new List<List<string>>();
+        //public List<Client> listOfClients = new List<Client>();
+
+
+        public FreelanceExchange(Admin admin_)
+        {
+            admin = admin_;
             Console.WriteLine("\nФриланс биржа создана!\n");
         }
 
-        string adminName(){ return admin_.name; }
-        void showAdminName(){ Console.WriteLine($"\nИмя админа: {admin_.name}\n"); }
-
-
-        people[] moderatorList(){ return moderatorList_; }
-        void showModeratorList()
+        public void showListOfOffers()
         {
-            Console.WriteLine($"\n Список модераторов: имя - стаж");
-            for (int i = 0; i < moderatorList_.Length; i++)
+            Console.WriteLine($"\nСписок предложений: имя исполнителя - предлагаемая услуга - цена");
+            for (int i = 0; i < listOfOffers.Count; i++)
             {
-                moderator t = moderatorList_[i];
-                Console.WriteLine($"{i}. {t.name}, {t.experience}");
+                var t = listOfOffers[i];
+                Console.WriteLine($"{i}. {t[0]} - {t[1]} - {t[2]}");
             }
             Console.WriteLine();
         }
-
-
-        void showListOfOffers()
-        {
-            Console.WriteLine($"\nСписок предложений: имя исполнителя - предлагаемая услуга - цена - стаж");
-            for (int i = 0; i < ListOfOffers_.Length; i++)
-            {
-                string[] t = ListOfOffers_[i];
-                Console.WriteLine($"{i}. {t[0]}, {t[1]}, {t[2]}, {t[3]}");
-            }
-            Console.WriteLine();
-        }
-        worker[] ListOfWorkers() { return ListOfWorkers_; }
-        void showListOfWorkers()
+        /*void showListOfWorkers()
         {
             Console.WriteLine($"\nСписок исполнителей: имя - стаж - количество выполненых работ");
-            for (int i = 0; i < ListOfWorkers_.Length; i++)
+            for (int i = 0; i < listOfWorkers_.Length; i++)
             {
-                worker t = ListOfWorkers_[i];
+                Worker t = listOfWorkers_[i];
                 Console.WriteLine($"{i}. {t.name} - {t.experience} - {t.score}");
             }
             Console.WriteLine();
-        }
+        }*/
 
-        void showListOfRequests()
+        public void showListOfRequests()
         {
             Console.WriteLine($"\nСписок предложений: имя заказчика - требуемая услуга - цена");
-            for (int i = 0; i < ListOfRequests_.Length; i++)
+            for (int i = 0; i < listOfRequests.Count; i++)
             {
-                string[] t = ListOfRequests_[i];
-                Console.WriteLine($"{i}. {t[0]}, {t[1]}, {t[2]}");
+                var t = listOfRequests[i];
+                Console.WriteLine($"{i}. {t[0]} - {t[1]} - {t[2]}");
             }
             Console.WriteLine();
         }
-        client[] ListOfClient() { return ListOfClients_; }
-        void showListOfClient()
+        /*void showListOfClient()
         {
             Console.WriteLine($"\nСписок заказчиков: имя - опубликовано заявок");
-            for (int i = 0; i < ListOfClients_.Length; i++)
+            for (int i = 0; i < listOfClients_.Length; i++)
             {
-                client t = ListOfClients_[i];
+                Client t = listOfClients_[i];
                 Console.WriteLine($"{i}. {t.name} - {t.numberOfRequests}");
+            }
+            Console.WriteLine();
+        }*/
+        public void showModeratorList()
+        {
+            Console.WriteLine($"\n Список модераторов: имя");
+            for (int i = 0; i < moderatorList.Count; i++)
+            {
+                var t = moderatorList[i];
+                Console.WriteLine($"{i}. {t.getName()}");
             }
             Console.WriteLine();
         }
