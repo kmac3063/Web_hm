9ad02411b6442d21ebfdc846020088ef9391f27c
kmac3063 <lantcov.iiu@students.dvfu.ru>
2019-12-12 19:43:51 +1000

    7/8

--- hm_3/3.7/Startup.cs
+++ hm_3/3.7/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+
 namespace _3._7
 {
     public class Startup
@@ -16,61 +17,96 @@ namespace _3._7
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDistributedMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 
         public interface IMessageSender
         {
-            string Request(string text);
-            void Send(string text);
+            string Request(HttpContext contex, string text);
+            void Send(HttpContext context, string key, string value);
         }
 
-        class EmailMessageSender : IMessageSender
+        class EmailMessageSender : IMessageSender // Сессии
         {
-            public string Request(HttpContext context, string text)
+            public string Request(HttpContext context, string key)
             {
-                //context.Request // Передавать это, в нём иметь дело с куки
-                return text;
+                var s = "EMPTY";
+                if (context.Session.Keys.Contains(key))
+                {
+                    s = context.Session.GetString(key);
+                }
+
+                return s;
             }
 
-            public void Send(string text)
+            public void Send(HttpContext context, string key, string value)
             {
-                text = "21";
+                context.Session.SetString(key, value);
             }
         }
 
-        public class SmsMessageSender : IMessageSender
+        public class SmsMessageSender : IMessageSender // Куки
         {
-            public string Request(string text)
+            public string Request(HttpContext context, string key)
             {
-                return text;
+                string s;
+                if (context.Request.Cookies.TryGetValue(key, out s))
+                {
+                    return s;
+                }
+
+                return "EMPTY";
             }
 
-            public void Send(string text)
+            public void Send(HttpContext context, string key, string value)
             {
-
+                try
+                {
+                    context.Response.Cookies.Append(key, value);
+                }
+                 catch(Exception e)
+                {
+                    Console.WriteLine("AAAAAAAAAAAAAA" + e.ToString() + "AAAAAAAAAAAAAA");
+                }
             }
         }
 
         public class MessageService
         {
-            IMessageSender Im;
-            public MessageService(IMessageSender Im)
+            public void Send(IMessageSender Im, HttpContext context, string key, string value)
             {
-                this.Im = Im;
-            }
-
-            public void Send(string text)
-            {
-                Im.Send(text);
+                Im.Send(context, key, value);
             }
         }
 
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            IMessageSender sms = new SmsMessageSender(app);
+            app.UseSession();
+
+            IMessageSender email = new EmailMessageSender(); //session
+            IMessageSender sms = new SmsMessageSender(); //cookies
+
+            var msg = new MessageService();
+
+            string s = "1000";
+            string key = s;
+            string value = s;
+
+            app.Run(async (context) =>
+            {
+                var emailString = email.Request(context, key);
+                var smsString = sms.Request(context, key);
+
+                msg.Send(sms, context, key, value);
+                msg.Send(email, context, key, value);
+
+                await context.Response.WriteAsync(emailString + '\n' + smsString);
+
+            });
         }
     }
 }
