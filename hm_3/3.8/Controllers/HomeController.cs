e9937603786b685108c8888b0d278e61ec995368
kmac3063 <lantcov.iiu@students.dvfu.ru>
2019-12-14 09:19:38 +1000

    8/8

---
+++ hm_3/3.8/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using _3._8.Models;
+
+namespace _3._8.Controllers
+{
+    public class HomeController : Controller
+    {
+        MobileContext db;
+        public HomeController(MobileContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            return View(db.Phones.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Buy(int? id)
+        {
+            if (id == null) return RedirectToAction("Index");
+            ViewBag.PhoneId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            db.Orders.Add(order);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо, " + order.User + ", за покупку!";
+        }
+    }
+}
